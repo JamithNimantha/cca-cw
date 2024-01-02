b3f22522f13ef51fe4e0c01bf1aa3a3be98f4f3e
Roshana Pitigala <23375478+pitigalari@users.noreply.github.com>
2024-01-02 18:26:38 +0530

    0.3 to main (#1)

--- src/js/main.js
+++ src/js/main.js
@@ -1,87 +1,43 @@
-jQuery(document).ready(function($) {
+(function($) {
+	function hero() {
 
-	'use strict';
+		var window_w = $(window).innerWidth();
+		
+		if(window_w > 1300) {
+			var pana_w = 180;
+		} else if( window_w > 1200 ) {
+			var pana_w = 100;
+		} else {
+			var pana_w = 60;
+		}
 
-        $(window).load(function() { // makes sure the whole site is loaded
-            $(".seq-preloader").fadeOut(); // will first fade out the loading animation
-            $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
-        })
-      
-        
-        $(function() {
-  
-        function showSlide(n) {
-            // n is relative position from current slide
-          
-            // unbind event listener to prevent retriggering
-            $body.unbind("mousewheel");
-          
-            // increment slide number by n and keep within boundaries
-            currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
-            
-            var displacment = window.innerWidth*currSlide;
-            // translate slides div across to appropriate slide
-            $slides.css('transform', 'translateX(-' + displacment + 'px)');
-            // delay before rebinding event to prevent retriggering
-            setTimeout(bind, 700);
-            
-            // change active class on link
-            $('nav a.active').removeClass('active');
-            $($('a')[currSlide]).addClass('active');
-            
-        }
-      
-        function bind() {
-             $body.bind('false', mouseEvent);
-          }
-      
-        function mouseEvent(e, delta) {
-            // On down scroll, show next slide otherwise show prev slide
-            showSlide(delta >= 0 ? -1 : 1);
-            e.preventDefault();
-        }
-        
-        $('nav a, .main-btn a').click(function(e) {
-            // When link clicked, find slide it points to
-            var newslide = parseInt($(this).attr('href')[1]);
-            // find how far it is from current slide
-            var diff = newslide - currSlide - 1;
-            showSlide(diff); // show that slide
-            e.preventDefault();
-        });
-      
-        $(window).resize(function(){
-          // Keep current slide to left of window on resize
-          var displacment = window.innerWidth*currSlide;
-          $slides.css('transform', 'translateX(-'+displacment+'px)');
-        });
-        
-        // cache
-        var $body = $('body');
-        var currSlide = 0;
-        var $slides = $('.slides');
-        var $slide = $('.slide');
-      
-        // give active class to first link
-        $($('nav a')[0]).addClass('active');
-        
-        // add event listener for mousescroll
-        $body.bind('false', mouseEvent);
-    })        
+		var hero_w = $('.hero-section').innerWidth();
+		var pa_length = (($('.pana-accordion-item').length - 1) * pana_w);
+		var hero_iw = hero_w - pa_length;
 
+		var window_h = $(window).innerHeight();
+		$('.pana-accordion-item').each(function() {
+			$(this).height(window_h);
+		});
+		accordion.init({
+			id: 'accordion',
+			expandWidth: hero_iw,
+			itemWidth: pana_w,
+			autoPlay: false,
+			borderWidth: 0
+		});
+		
+	}
+	
+	hero();
 
-        $('#form-submit .date').datepicker({
-        });
+	/*------------------
+		Background Set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+	
+})(jQuery);
 
-
-        $(window).on("scroll", function() {
-            if($(window).scrollTop() > 100) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in your css)
-               $(".header").removeClass("active");
-            }
-        });
-
-
-});
